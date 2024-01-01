a9ea1895f0989dfb9adca1a2dc3e473261f906ed
Vidit <vdsavaliya19@gmail.com>
2024-01-01 07:42:27 +0530

    Initial Commit

---
+++ IBOutletsAndAction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBOutletsAndAction
+//
+//  Created by R&W on 01/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
